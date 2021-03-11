cd5a73451c9b3168ea4b30c9e4c4aa1e0da0c7c8
970862 <53192496+970862@users.noreply.github.com>
2021-03-11 23:01:11 +0530

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,13 @@
+print("This is march month")
+print('this is march month')
+print("surbhi's fiance name is archit")
+print('"we have seen "this" example everlier"')
+print('surbhi' + "archit")
+a = "archit"
+b = "surbhi"
+# lets see what happends
+print(a,b)
+print(a + " " + b)
+bride = input("enter bride;s name")
+groom = input("enter grooms name")
+print(bride, "marries", " ",groom)
